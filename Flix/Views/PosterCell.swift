249f11f16e95d1ce65e642af75b9504c4e19b3c1
johnjing <838633350@qq.com>
2018-02-10 13:45:12 -0800

    Basics done

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by JY on 2018/2/7.
+//  Copyright © 2018年 JY. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var PosterImageView: UIImageView!
+    
+}
